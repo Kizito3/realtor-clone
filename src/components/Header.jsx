64a091341d2ad71d37a591bed03c853cac00dcd3
unknown <chigokizzy@gmail.com>
2023-04-30 14:36:57 +0100

    created the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
     // init the usenavigate by putting it in a variable
     const navigate = useNavigate()
   return (
-    <div className='bg-white border-b shadow-xl sticky top-0 z-50 py-4'>
+    <div className='bg-white border-b shadow-xl sticky top-0 z-40 py-4'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Logo" className='h-6 cursor-pointer' onClick={()=> navigate("/")} />
